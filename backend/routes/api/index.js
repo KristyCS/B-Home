9c7cf44b03752db2ab1eff56ed0da58127c112dd
Kristy Zhang <zhangjy1988@gmail.com>
2021-10-07 16:50:22 -0400

    log in

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,28 +1,14 @@
+// backend/routes/api/index.js
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 
-const asyncHandler = require('express-async-handler');
-const { setTokenCookie, requireAuth } = require('../../utils/auth.js');
-const { User } = require('../../db/models');
-router.get('/set-token-cookie', asyncHandler(async (req, res) => {
-  const user = await User.findOne({
-      where: {
-        username: 'Demo-lition'
-      },
-    })
-  setTokenCookie(res, user);
-  return res.json({ user });
-}));
+router.use('/session', sessionRouter);
 
-router.get(
-  '/require-auth',
-  requireAuth,
-  (req, res) => {
-    return res.json(req.user);
-  }
-);
+router.use('/users', usersRouter);
 
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
+});
 
-router.post('/test', function(req, res) {
-    res.json({ requestBody: req.body });
-  });
 module.exports = router;
