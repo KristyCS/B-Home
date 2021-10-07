080db47eaa88dcc31363986bf987d8cd44521868
Kristy Zhang <zhangjy1988@gmail.com>
2021-10-07 16:59:08 -0400

    auth

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -35,4 +35,18 @@ router.delete("/", (_req, res) => {
   res.clearCookie("token");
   return res.json({ message: "success" });
 });
+
+// Restore session user
+router.get(
+    '/',
+    restoreUser,
+    (req, res) => {
+      const { user } = req;
+      if (user) {
+        return res.json({
+          user: user.toSafeObject()
+        });
+      } else return res.json({});
+    }
+  );
 module.exports = router;
