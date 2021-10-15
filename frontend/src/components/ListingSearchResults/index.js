61192fa89046ac96745eea5b1420238675373b63
Kristy Zhang <zhangjy1988@gmail.com>
2021-10-15 15:15:06 -0400

    naming

--- frontend/src/components/ListingSearchResults/index.js
+++ frontend/src/components/ListingSearchResults/index.js
@@ -1,1 +1,46 @@
-export { default } from './listingSearchResults';
+// Import hooks from 'react'. Which hook is meant for causing side effects?
+import { useEffect, useState, useMemo } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import styles from "./ListingSearchResults.module.css";
+import { NavLink } from "react-router-dom";
+import { getListings } from "../../store/listing";
+import SingleListingSearchResult from "../SingleListingSearchResult/singleListingSearchResult";
+
+const ListingSearchResults = () => {
+  const dispatch = useDispatch();
+  const region = localStorage.getItem("region");
+  const [listingsAtRegion, setListingAtRegion] = useState([]);
+  const listingsObj = useSelector((state) => state.listing);
+  const listings = useMemo(() => {
+    return Object.values(listingsObj);
+  }, [listingsObj]);
+  useEffect(() => {
+    dispatch(getListings());
+  }, [dispatch]);
+
+  useEffect(() => {
+    const targetListings = [];
+    for (const listing of listings) {
+      if (listing.Location.city === region) {
+        targetListings.push(listing);
+      }
+    }
+    setListingAtRegion(targetListings);
+  }, [listings]);
+
+  return (
+    <>
+      <main>
+        <nav className={styles.container}>
+          {listingsAtRegion.map((listing) => {
+            return (
+              <SingleListingSearchResult key={listing.id} listing={listing} />
+            );
+          })}
+        </nav>
+      </main>
+    </>
+  );
+};
+
+export default ListingSearchResults;
