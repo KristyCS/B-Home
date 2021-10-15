340b1b44143ce0aed05200780d6aeef610e304b0
Kristy Zhang <zhangjy1988@gmail.com>
2021-10-15 14:22:44 -0400

    remove favicon

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Favicon from 'react-favicon'
+
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
