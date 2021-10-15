0eadb9013811aade5ca1df8088db90e7c4c4332f
Kristy Zhang <zhangjy1988@gmail.com>
2021-10-15 14:59:46 -0400

    naming

--- frontend/src/components/HomeBody/index.js
+++ frontend/src/components/HomeBody/index.js
@@ -1,1 +1,1 @@
-export { default } from './HomeBody';
+export { default } from './homeBody';
--- frontend/src/components/ListingDetails/index.js
+++ frontend/src/components/ListingDetails/index.js
@@ -1,1 +1,1 @@
-export { default } from './ListingDetails';
+export { default } from './listingDetails';
